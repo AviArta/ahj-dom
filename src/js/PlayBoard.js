f4f271bce1267a1c4653501c551464832cf07b01
AviArta <buzlova.a@yandex.ru>
2024-07-26 20:15:24 +0300

    add Board

--- src/js/PlayBoard.js
+++ src/js/PlayBoard.js
@@ -4,14 +4,34 @@ export default class PlayBoard {
   }
 
   createBoard() {
-    const board = document.createElement("div");
+    let board = document.createElement("div");
     board.classList.add("board");
     for (let i = 0; i < 16; i += 1) {
-      const cell = document.createElement("div");
+      let cell = document.createElement("div");
       cell.classList.add("cell");
       cell.dataset.index = i + 1;
       board.appendChild(cell);
     }
     this._element.appendChild(board);
+  }
+
+  createGoblin() {
+    let cells = Array.from(this._element.querySelector(".board").children);
+    let goblinCell = this._element.querySelector(".cell_selected");
+    let lastIndex = 0;
+    let index = 0;
+    if (!goblinCell) {
+      index = Math.floor(Math.random() * cells.length);
+      lastIndex = index;
+    } else {
+      index = Math.floor(Math.random() * cells.length);
+      if (lastIndex !== index) {
+        cells.forEach((cell) => cell.classList.remove("cell_selected"));
+      } else {
+        index = Math.floor(Math.random() * cells.length);
+        cells.forEach((cell) => cell.classList.remove("cell_selected"));
+      }
+    }
+    cells[index].classList.add("cell_selected");
   }
 }
