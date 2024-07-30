89c3039452980ad272e3af1e73d4262d38203155
AviArta <buzlova.a@yandex.ru>
2024-07-30 20:42:33 +0300

    Updates

--- main.js
+++ main.js
@@ -2,21 +2,52 @@
 /******/ 	"use strict";
 var __webpack_exports__ = {};
 
-;// CONCATENATED MODULE: ./src/js/app.js
-// TODO: write code here
-
-// comment this to pass build
-const unusedVariable = "variable";
-
-// for demonstration purpose only
-function demo(value) {
-  return `Demo: ${value}`;
+;// CONCATENATED MODULE: ./src/js/PlayBoard.js
+class PlayBoard {
+  constructor(element) {
+    this._element = element;
+  }
+  createBoard() {
+    let board = document.createElement("div");
+    board.classList.add("board");
+    for (let i = 0; i < 16; i += 1) {
+      let cell = document.createElement("div");
+      cell.classList.add("cell");
+      cell.dataset.index = i + 1;
+      board.appendChild(cell);
+    }
+    this._element.appendChild(board);
+  }
+  createGoblin() {
+    let cells = Array.from(this._element.querySelector(".board").children);
+    let goblinCell = this._element.querySelector(".cell_selected");
+    let lastIndex = 0;
+    let index = 0;
+    if (!goblinCell) {
+      index = Math.floor(Math.random() * cells.length);
+      lastIndex = index;
+    } else {
+      index = Math.floor(Math.random() * cells.length);
+      if (lastIndex !== index) {
+        cells.forEach(cell => cell.classList.remove("cell_selected"));
+      } else {
+        index = Math.floor(Math.random() * cells.length);
+        cells.forEach(cell => cell.classList.remove("cell_selected"));
+      }
+    }
+    cells[index].classList.add("cell_selected");
+  }
 }
-console.log("app.js included");
+;// CONCATENATED MODULE: ./src/js/app.js
+
+
+const board = new PlayBoard(document.querySelector(".board-container"));
+document.addEventListener("DOMContentLoaded", () => {
+  board.createBoard();
+  setInterval(() => board.createGoblin(), 2000);
+});
 ;// CONCATENATED MODULE: ./src/index.js
 
 
-
-// TODO: write your code in app.js
 /******/ })()
 ;
