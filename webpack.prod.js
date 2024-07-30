37b9b2ee190bf074c6475f7196f7e80e88947991
AviArta <buzlova.a@yandex.ru>
2024-07-30 14:55:57 +0300

    Add change

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,33 +1,5 @@
-const common = require("./webpack.common");
+
 const { merge } = require("webpack-merge");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
-module.exports = merge(common, {
-  mode: "production",
-  devtool: false,
-  output: {
-    filename: "main-[contenthash].js",
-    assetModuleFilename: "css/[name]-[hash][ext]",
-  },
-  plugins: [new MiniCssExtractPlugin()],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.jpe?g$|\.gif$|\.png$|\.PNG$|\.svg$/,
-        type: "asset/resource",
-        generator: {
-          filename: "images/[name]-[hash][ext]",
-        },
-      },
-    ],
-  },
-});
-
-/*const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
@@ -36,4 +8,4 @@ module.exports = merge(common, {
   optimization: {
     minimizer: [new CssMinimizerPlugin()],
   },
-});*/
+});
