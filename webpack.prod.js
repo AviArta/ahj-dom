bb9cf71fe6201e2581096fb454d2d6dfd2180b3f
AviArta <buzlova.a@yandex.ru>
2024-07-30 13:12:46 +0300

    No CSS error

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,34 @@
+//const path = require("path");
+const common = require("./webpack.common");
 const { merge } = require("webpack-merge");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = merge(common, {
+  mode: "production",
+  devtool: false,
+  output: {
+    filename: "main-[contenthash].js",
+    assetModuleFilename: "css/[name]-[hash][ext]",
+  },
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.jpe?g$|\.gif$|\.png$|\.PNG$|\.svg$/,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name]-[hash][ext]",
+        },
+      },
+    ],
+  },
+});
+
+/*const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
@@ -7,4 +37,4 @@ module.exports = merge(common, {
   optimization: {
     minimizer: [new CssMinimizerPlugin()],
   },
-});
+});*/
